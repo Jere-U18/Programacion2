d848018fffe83b67f06b594c979a3ec957eb38e3
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2026-05-04 17:50:00 -0500

    primera subida

---
+++ src/main/java/com/uce/Main.java
@@ -0,0 +1,26 @@
+package com.uce;
+
+import com.uce.funciones.calculos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        calculos.suma(1,2);
+
+        calculos.resta(4, 2000);
+
+        calculos.multiplicacion(1,2);
+
+        calculos.division(4, 3);
+
+        calculos.factorial(6);
+
+        calculos.piE();
+        
+
+
+
+
+
+    }
+}
